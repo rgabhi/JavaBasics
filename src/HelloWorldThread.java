0ef634c9c2dd65a06d1b6091787461b0bf8939a0
Abhinav Gupta <abhinavrgupta98@gmail.com>
2024-01-04 10:00:05 +0530

    add thread class

---
+++ src/HelloWorldThread.java
@@ -0,0 +1,7 @@
+public class HelloWorldThread implements Runnable{
+    @Override
+    public void run(){
+        System.out.println("Hello World from " + Thread.currentThread().getName());
+    }
+
+}
