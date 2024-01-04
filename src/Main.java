25d55a4a4d2da5128c75bfe32c5a3cfa7d7ee8ec
Abhinav Gupta <abhinavrgupta98@gmail.com>
2024-01-04 10:00:50 +0530

    run threads

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+        HelloWorldThread helloWorldThread = new HelloWorldThread();
+        Thread t1 = new Thread(helloWorldThread);
+        Thread t2 = new Thread(helloWorldThread);
+        t1.start();
+        t2.start();
     }
 }
